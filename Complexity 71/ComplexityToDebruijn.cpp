605dd1f2e078022abb2ec9c8143abbeb77b37c43
RazanDally <90503892+RazanDally@users.noreply.github.com>
2023-06-28 12:20:59 +0300

    Complexity to debruijn (needs work takes too long to run)

--- Complexity 71/ComplexityToDebruijn.cpp
+++ Complexity 71/ComplexityToDebruijn.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "ComplexityToDebruijn.h"
+#include "set"
 int n;
 
 ComplexityToDebruijn::ComplexityToDebruijn(int complexity, int order) : complexity(complexity), order(order), total_seq_num(0) {}
@@ -54,9 +55,9 @@ void ComplexityToDebruijn::compute() {
     }
 }
 
-static void generateXORStrings(const string& s, string& a, string& b, int index, vector<string>& options) {
+static void generateXORStrings(const string& s, string& a, string& b, int index, vector<pair<string,string>>& options) {
     if (index == s.size()) {
-        options.push_back(a + " " + b);
+        options.emplace_back(a, b);
         return;
     }
 
@@ -88,27 +89,110 @@ static void generateXORStrings(const string& s, string& a, string& b, int index,
     }
 }
 
-static vector<string> getAllXORStrings(const string& s) {
-    vector<string> options;
+static vector<pair<string,string>>  getAllXORStrings(const string& s) {
+    vector<pair<string,string>> options;
     string a, b;
     generateXORStrings(s, a, b, 0, options);
     return options;
 }
+bool comparePairs(const pair<string, string>& a, const pair<string, string>& b) {
+    return a.first < b.first;
+}
+
+static vector<string> generateStrings(const vector<pair<string, string>>& input) {
+    vector<string> result;
+    string prefix;
+    int inputSize = 8;
+
+    // Sort the input vector based on the first element of each pair
+    vector<pair<string, string>> sortedInput = input;
+    sort(sortedInput.begin(), sortedInput.end(), comparePairs);
+
+
+    // Generate all possible combinations of first 64 chars
+    do {
+        vector<bool> check(128, false);
+        bool debruijn = true;
+        prefix.clear();
+        for (int i = 0; i < inputSize; i++) {
+            auto val = sortedInput[i].first.substr(0, 7);
+            int index = binaryToDecimal(val);
+            int index2 = binaryToDecimal(sortedInput[i].first.substr(1, 7));
+            if(!check[index] && !check[index2] && index != index2){
+                check[index] = true;
+                check[index2] = true;
+            } else {
+                debruijn = false;
+                break;
+            }
+            prefix += sortedInput[i].first;
+        }
+        if (!debruijn) continue;
+        else {
+            cout << "im alive" << endl;
+        }
+        // Generate the corresponding string of the next 64 chars
+        string suffix;
+        for (int i = 0; i < inputSize; i++) {
+            int index = binaryToDecimal(sortedInput[i].second.substr(0, 7));
+            int index2 = binaryToDecimal(sortedInput[i].second.substr(1, 7));
+            if(!check[index] && !check[index2] && index != index2){
+                check[index] = true;
+                check[index2] = true;
+            } else {
+                debruijn = false;
+                break;
+            }
+            suffix += sortedInput[i].second;
+        }
+        if (!debruijn){
+            continue;
+        }else {
+            cout << "Actually made it " << endl;
+        }
+        // Combine prefix and suffix
+        result.push_back(prefix + suffix);
+
+    } while (next_permutation(sortedInput.begin(), sortedInput.end(), comparePairs));
+
+    return result;
+}
+//static vector<string> generateDebruijn( const vector<pair<string,string>>& options, int idx,set<string> used, const vector<bool> check, int length, const string& A, const string& B){
+//    if(A.length() == length || B.length() == length){
+//        if(validate(A+B))
+//            return {A+B};
+//    }
+//    if (used.contains(options[idx].first)){
+//        return generateDebruijn(options, )
+//    }
+//}
 
 ll ComplexityToDebruijn::fromSubseqToDebruijn(string seq) {
     ll count = 0;
-    vector<bool> check(pow(2, this->order), false);
-    vector<string> options;
-    string a, b;
-    generateXORStrings(seq, a, b, 0, options);
+    vector<pair<string,string>> options = getAllXORStrings(seq);
     std::vector<std::string> filtered_de_bruijn;
+    vector<pair<string,string>> filtered_options;
     n = this->order;
-//    std::copy_if(options.begin(), options.end(), std::back_inserter(filtered_de_bruijn), [](const std::string& s) {
-//        return validate(s);
-//    }); //wrong need to do after building all of the seq.
-    cout << options[1] << endl;
-//    for (auto s: filtered_de_bruijn) {
-//        cout << s << endl;
-//    }
+    std::copy_if(options.begin(), options.end(), std::back_inserter(filtered_options), [](const pair<string,string>& s) {
+        int i_f1 = binaryToDecimal(s.first.substr(0, 7));
+        int i_f2 = binaryToDecimal(s.first.substr(1, 7));
+        int i_s1 = binaryToDecimal(s.second.substr(0, 7));
+        int i_s2 = binaryToDecimal(s.second.substr(1, 7));
+        return i_f1 != i_f2 && i_s1 != i_s2 && i_f1 != i_s2 && i_s1 != i_f2 && i_s2 != i_f2 && i_s1 != i_f1;
+    });
+    auto all_sequences = generateStrings(filtered_options);
+    std::copy_if(all_sequences.begin(), all_sequences.end(), std::back_inserter(filtered_de_bruijn), [](const std::string& s) {
+        return validate(s);
+    });
+    int i = 0;
+    for (const auto& s : all_sequences) {
+        i++;
+        cout << s << endl;
+        if(i == 10){
+            break;
+        }
+    }
+//    set<string> used;
+//    generateDebruijn(options, 0, used, check, pow(2, this->order - 1), "", "");
     return filtered_de_bruijn.size();
 }
--- Complexity 71/main.cpp
+++ Complexity 71/main.cpp
@@ -9,10 +9,10 @@ using namespace std;
 int main(){
     ComplexityToDebruijn C(7, 7);
     C.fromSubseqToDebruijn("00000011");
-    SequenceGenerator seq(7);
-    auto s = seq.getSequences();
-    for (const auto& i : s) {
-        cout << i << endl;
-    }
-    cout << seq.getNumOfSeq() << endl;
+//    SequenceGenerator seq(7);
+//    auto s = seq.getSequences();
+//    for (const auto& i : s) {
+//        cout << i << endl;
+//    }
+//    cout << seq.getNumOfSeq() << endl;
 }
