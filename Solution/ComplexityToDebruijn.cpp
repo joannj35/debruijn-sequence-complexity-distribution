24556bc1b08830aec18874ee4adca7d2793830fb
RazanDally <90503892+RazanDally@users.noreply.github.com>
2023-08-03 17:07:41 +0300

    turning the code to be GPU compatible. * String to char* * vector to array

--- Solution/ComplexityToDebruijn.cpp
+++ Solution/ComplexityToDebruijn.cpp
@@ -1,38 +1,30 @@
 #include "ComplexityToDebruijn.h"
 #include "set"
-int n;
-#include <omp.h>
 #include <fstream>
 #include <chrono>
+#include <omp.h>
+#include <cstdio>
+#include <cstring>
+
 using namespace std::chrono;
 map<string, int> bin_to_dec1;
+#define MAX_SIZE 4194304
+//When order == 6 DB_SIZE = 65
+#define DB_SIZE 129
+
+#pragma omp declare target
+int n;
+#pragma omp end declare target
 
 ComplexityToDebruijn::ComplexityToDebruijn(int complexity, int order) : order(order), total_seq_num(0) {
     this->complexity = complexity;
     this->sub_complexity = this->complexity - pow(2,order - 1);
 }
 
-map<string, int> generateStringMap() {
-    map<string, int> stringMap;
-
-    for (int i = 0; i < pow(2,n); i++) {
-        string binaryString;
-        int value = i;
-
-        for (int j = 0; j < n; j++) {
-            binaryString = (char)('0' + (value % 2)) + binaryString;
-            value /= 2;
-        }
-
-        stringMap[binaryString] = i;
-    }
-
-    return stringMap;
-}
-
-static int binaryToDecimal(const string& binaryString) {
+#pragma omp declare target
+static int binaryToDecimal(const char binaryString[]) {
     int decimalValue = 0;
-    int length = binaryString.length();
+    int length = strlen(binaryString);
 
     for (int i = 0; i < length; ++i) {
         if (binaryString[i] == '1') {
@@ -42,51 +34,54 @@ static int binaryToDecimal(const string& binaryString) {
 
     return decimalValue;
 }
+#pragma omp end declare target
 
-static bool validate(string seq){
-    int seq_size = pow(2, n);
-    if(seq.length() != seq_size){
+#pragma omp declare target
+bool ComplexityToDebruijn::isRotation(const char* s1, const char* s2) {
+    size_t len1 = strlen(s1);
+    size_t len2 = strlen(s2);
+
+    if (len1 != len2) {
         return false;
     }
-    vector<bool> sub_seq(seq_size, false);
-    string rotation = seq;
-    for (int i = 0; i < n; ++i) {
-        rotation += seq[i];
-    }
-    string val = "";
-    for (int i = 0; i < seq_size+n; ++i) {
-        if(i >= n){
-            int index = binaryToDecimal(val);
-            if(!sub_seq[index]){
-                sub_seq[index] = true;
-            } else {
-                return false;
-            }
-            val = val.substr(1);
-        }
-        val += rotation[i];
-    }
-    return ranges::all_of(sub_seq, [](bool i) { return i; });
-}
 
-bool ComplexityToDebruijn::isRotation(const std::string& s1, std::string s2)
-{
-    if (s1.size() != s2.size())
-        return false;
-
-    if (s1 == s2)
+    if (strcmp(s1, s2) == 0) {
         return true;
+    }
 
-    string temp = s1 + s1;
-    return (temp.find(s2) != string::npos);
+    char* temp = new char[2 * len1 + 1];  // +1 for the null terminator
+    strcpy(temp, s1);
+    strcat(temp, s1);
+
+    bool result = (strstr(temp, s2) != nullptr);
+
+    delete[] temp;  // free the allocated memory
+    return result;
+}
+#pragma omp end declare target
+
+static vector<char**> convert_to_char_ptrs(int num_batches ,vector<string> batches) {
+    vector<char**> results;
+    int batch_size = MAX_SIZE;
+    for (int j = 0; j < num_batches; j++) {
+        int start = j * batch_size;
+        char** batch_data = new char*[batch_size];
+        for (size_t i = 0; i < batch_size; ++i) {
+            batch_data[i] = new char[batches[start+i].size() + 1];
+            strcpy(batch_data[i], batches[start + i].c_str());
+        }
+        results.push_back(batch_data);
+    }
+    return results;
 }
 
 void ComplexityToDebruijn::compute() {
+    n = this->order;
     cout << "small sequence start..." << endl;
     auto start = std::chrono::high_resolution_clock::now();
     SequenceGenerator sub_sequences(this->sub_complexity);
     auto sub_seq =sub_sequences.getSequences();
-    vector<pair<string,ll>> subseq_to_db(sub_seq.size());
+    ll* subseq_to_db = new ll[sub_seq.size()];
     this->up_to_1000 = vector<vector<string>>(sub_seq.size());
     int i;
     auto end = std::chrono::high_resolution_clock::now();
@@ -95,30 +90,53 @@ void ComplexityToDebruijn::compute() {
     std::ofstream fileout("order_"+ to_string(order)+"_complexity_"+ to_string(complexity) +"_omp.txt");
     cout << "For order "<< order << " complexity "<< complexity<< ":" << endl;
     start = std::chrono::high_resolution_clock::now();
-    #pragma omp parallel for schedule(dynamic) shared(subseq_to_db,sub_seq,n,cout,fileout) private(i) default(none)
-    for(i = 0; i < sub_seq.size(); i++) {
-        auto seq = sub_seq[i];
-        string x = seq + seq;
-        if (seq.size() <= 16) {
-            x += seq + seq;
-        }
-        if (seq.size() == 8) {
-            x += seq + seq + seq + seq;
-        }
-        vector<string> db_seq;
-        ll num = fromSubseqToDebruijn(x,db_seq);
 
-        #pragma omp critical
-        {
+    //-------------------GPU prep-----------------
+    int num_of_batches = ceil(sub_seq.size()/MAX_SIZE);
+    char* up_to_1000_batch[MAX_SIZE][1000];
+    vector<char**> seq_batches = convert_to_char_ptrs(num_of_batches,sub_seq);
+    ll sub_seq_to_debruijn[MAX_SIZE];
+    for (auto batch : seq_batches){
+        #pragma omp parallel for schedule(dynamic) shared(batch,subseq_to_db,sub_seq,n,sub_seq_to_debruijn,up_to_1000_batch) private(i) default(none)
+        for(i = 0; i < MAX_SIZE; i++) {
+            char* seq = batch[i];
+            if (seq == nullptr) continue;
+            char x[65];
+            strcpy(x, seq);
+            strcat(x, seq);
+
+            if (strlen(seq) <= 16) {
+                strcat(x, seq);
+                strcat(x, seq);
+            }
+            if (strlen(seq) == 8) {
+                strcat(x, seq);
+                strcat(x, seq);
+                strcat(x, seq);
+                strcat(x, seq);
+            }
+            char db_seq[MAX_SIZE][DB_SIZE];
+            ll num = fromSubseqToDebruijn(x,db_seq);
+            sub_seq_to_debruijn[i] = num;
+            for (int j = 0; j < 1000; j++) {
+                up_to_1000_batch[i][j] = db_seq[j];
+            }
+        }
+        for (int j = 0; j < this->subseq_to_debruijn.size(); ++j) {
+            string seq = batch[j];
+            ll num = sub_seq_to_debruijn[j];
+            auto db_seq = up_to_1000_batch[j];
             fileout << "Debruijn Sequences generated by the sub sequence " << seq << " : " << endl;
-            for (const auto &str: db_seq) {
+            for (int k = 0; (k < 1000 && k < num); k++) {
+                string str = db_seq[k];
                 fileout << str << endl;
             }
             fileout << "the number of Debruijn sequences: " << num << endl << endl;
-            total_seq_num += num;
+            this->total_seq_num += num;
             cout << "Sequence #" << i << ": " << seq << " - " << num << endl;
         }
     }
+
     fileout << "number of small sequences with complexity " << complexity - pow(2,order - 1)<< " is: " << sub_seq.size() << endl;
     fileout << "total number of sequences of complexity " << complexity << " is: " << total_seq_num << endl;
     end = high_resolution_clock::now();
@@ -140,140 +158,140 @@ void ComplexityToDebruijn::compute() {
         cout << "overall execution time is " << duration.count() << " seconds" << endl;
         fileout << "overall execution time is " << duration.count() << " seconds" << endl;
     }
+    delete[] subseq_to_db;
 }
 
-void ComplexityToDebruijn::generateXORStrings(const string& s, string& a, string& b, int index, vector<pair<string,string>>& options, vector<bool> check, vector<string>& db_seq) {
-    if (index == s.size()) {
-        //if(options.size() % 1000 == 0 && options.size() != 0){
-          //  cout << "Current size = " << options.size() << endl;
-        //}
-        auto a_b = a + b;
-        auto b_a = b + a;
+#pragma omp declare target
+static char* substring(const char* source, int start, int length) {
+    char* result = new char[length + 1];  // +1 for the null terminator
+    strncpy(result, source + start, length);
+    result[length] = '\0';  // null terminate the resulting substring
+    return result;
+}
+#pragma omp end declare target
+
+#pragma omp declare target
+static bool find_opposite(char options_a[][64], char options_b[][64],char a[], char b[], ll options_size){
+    for (int i = 0; i < options_size; ++i) {
+        if ((strcmp(options_a[i], b) == 0) && ((strcmp(options_b[i], a) == 0)))
+            return true;
+    }
+    return false;
+}
+#pragma omp end declare target
+
+#pragma omp declare target
+void ComplexityToDebruijn::generateXORStrings(const char s[], char a[], char b[], int index, char options[][DB_SIZE], bool check[], char db_seq[][DB_SIZE], ll &options_size) {
+    if (index == strlen(s)) {
+        char a_b[DB_SIZE];
+        strcpy(a_b,a);
+        strcat(a_b,b);
+        char b_a[DB_SIZE];
+        strcpy(b_a,b);
+        strcat(b_a,a);
         for (int i = n - 1; i > 0; i--) {
-            auto a_sub = binaryToDecimal(a_b.substr(a.size() - i, n));
-            auto b_sub = binaryToDecimal(b_a.substr(a.size() - i, n));
+            char* a_b_substr = substring(a_b, index - i, n);
+            char* b_a_substr = substring(b_a, index - i, n);
+            int a_sub = binaryToDecimal(a_b_substr);
+            int b_sub = binaryToDecimal(b_a_substr);
             if (!check[a_sub] && !check[b_sub] && a_sub != b_sub) {
                 check[a_sub] = true;
                 check[b_sub] = true;
             } else {
+                delete b_a_substr;
+                delete a_b_substr;
+                return;
+            }
+            delete b_a_substr;
+            delete a_b_substr;
+        }
+        for (int op = 0; op < options_size; op++) {
+            if (isRotation(a_b, options[op])) {
                 return;
             }
         }
-        if (find(options.begin(), options.end(), make_pair(b, a)) == options.end()) {
-            for (const auto &op: options) {
-                if (isRotation(a_b, op.first + op.second)) {
-                    return;
-                }
-            }
-            if (db_seq.size() < 1000)
-                db_seq.push_back(a + b);
-            options.emplace_back(a, b);
-            /*bool found_rotation = false;
-            #pragma omp parallel for shared(found_rotation, options, a_b) default(none)
-            for (int i = 0; i < options.size(); i++) {
-                if (found_rotation) {
-                    continue; // Skip the remaining iterations if found_rotation is true
-                }
-
-                const auto &op = options[i];
-                if (isRotation(a_b, op.first + op.second)) {
-                    #pragma omp critical
-                    {
-                        if (!found_rotation) { // Check again inside the critical section to avoid race conditions
-                            found_rotation = true;
-                        }
-                    }
-                }
-            }
-
-            if (!found_rotation) {
-                if (db_seq.size() < 1000) {
-                    // Ensure that the push_back operation is thread-safe
-                    #pragma omp critical
-                    {
-                        db_seq.push_back(a + b);
-                    }
-                }
-                options.emplace_back(a, b);
-            }*/
+        if (options_size < 1000){
+            strcpy(db_seq[options_size],a_b);
         }
+        strcpy(options[options_size],a_b);
+        options_size++;
         return;
     }
 
-    vector<pair<char, char>> combinations;
+    const int MAX_COMBINATIONS = 2;
+    const int CHARS_PER_COMBINATION = 2;
+
+    char combinations[MAX_COMBINATIONS][CHARS_PER_COMBINATION];
+
     if (s[index] == '0') {
-        combinations = {{'0', '0'}, {'1', '1'}};
+        combinations[0][0] = '0'; combinations[0][1] = '0';
+        combinations[1][0] = '1'; combinations[1][1] = '1';
     } else if (s[index] == '1') {
-        combinations = {{'1', '0'}, {'0', '1'}};
+        combinations[0][0] = '1'; combinations[0][1] = '0';
+        combinations[1][0] = '0'; combinations[1][1] = '1';
     }
 
-    for (const auto& combination : combinations) {
-        a += combination.first;
-        b += combination.second;
-        if (a.size() >= n) {
-            auto a_sub = binaryToDecimal(a.substr(a.size() - n, n));
-            auto b_sub = binaryToDecimal(b.substr(b.size() - n, n));
+
+    for (int c = 0; c < MAX_COMBINATIONS; ++c) {
+        char* combination = combinations[c];
+
+        // Append the combination characters to a and b
+        size_t len_a = strlen(a);
+        size_t len_b = strlen(b);
+        a[len_a] = combination[0];
+        b[len_b] = combination[1];
+        a[len_a + 1] = '\0';
+        b[len_b + 1] = '\0';
+
+        if (strlen(a) >= n) {
+            auto a_sub = binaryToDecimal(&a[strlen(a) - n]);
+            auto b_sub = binaryToDecimal(&b[strlen(b) - n]);
             if (!check[a_sub] && !check[b_sub] && a_sub != b_sub) {
                 check[a_sub] = true;
                 check[b_sub] = true;
-                generateXORStrings(s, a, b, index + 1, options, check, db_seq);
+                generateXORStrings(s, a, b, index + 1, options, check, db_seq, options_size);
                 check[a_sub] = false;
                 check[b_sub] = false;
             }
         } else {
-            generateXORStrings(s, a, b, index + 1, options, check,db_seq);
+            generateXORStrings(s, a, b, index + 1, options, check, db_seq, options_size);
         }
-        a.pop_back();
-        b.pop_back();
-    }
-}
 
+        // Remove last character from a and b
+        a[len_a] = '\0';
+        b[len_b] = '\0';
+    }
+
+}
+#pragma omp end declare target
 
 /*
  * TODO:
  * remove rotations in an efficient way
  */
-vector<pair<string,string>> ComplexityToDebruijn::getAllXORStrings(string s, vector<string>& db_seq) {
-//static ll  getAllXORStrings(const string& s) {
-    vector<pair<string,string>> options, filtered_options;
-//    ll options = 0;
-    string a = "", b = "";
-    vector<bool> check(pow(2,n), false);
-    //#pragma omp task private(s, a, b, options, bin_to_dec1,check) default(none)
-    generateXORStrings(s, a, b, 0, options, check,db_seq);
-    return options;
-}
+#pragma omp declare target
+void ComplexityToDebruijn::getAllXORStrings(const char s[], char db_seq[MAX_SIZE][DB_SIZE],ll &options_size) {
 
-ll ComplexityToDebruijn::fromSubseqToDebruijn(string seq, vector<string>& db_seq) {
-    ll count = 0;
-    n = this->order;
-    //bin_to_dec1 = generateStringMap();
-    vector<pair<string,string>> options;
-//
-//    #pragma omp task private(seq, options, bin_to_dec1) default(none)
-    options = getAllXORStrings(seq,db_seq);
-//    for(auto p : options){
-//        cout << "(" << p.first+p.second << ")" << endl;
-//    }
-
-    return options.size();
-}
-
-vector<string> ComplexityToDebruijn::removeRotations(const vector<string> &sequences) {
-    vector<string> distinct_sequences;
-    for (auto seq:sequences) {
-        bool rotation = false;
-        for (auto d_sub:distinct_sequences){
-            if(isRotation(d_sub,seq)){
-                rotation = true;
-                break;
-            }
-        }
-        if (!rotation){
-            distinct_sequences.emplace_back(seq);
-        }
+    char options[MAX_SIZE][DB_SIZE];
+    char a[65], b[65];
+    a[0] = '\0';
+    b[0] = '\0';
+    bool check[DB_SIZE];
+    for (bool & i : check) {
+        i = false;
     }
-    return distinct_sequences;
+    //#pragma omp task private(s, a, b, options, bin_to_dec1,check) default(none)
+    generateXORStrings(s, a, b, 0, options, check,db_seq, options_size);
+}
+#pragma omp end declare target
+
+ll ComplexityToDebruijn::fromSubseqToDebruijn(const char seq[], char db_seq[MAX_SIZE][DB_SIZE]) {
+    ll count = 0;
+    vector<pair<string,string>> options;
+    ll options_size = 0;
+    getAllXORStrings(seq,db_seq,options_size);
+
+    return options_size;
 }
 
 const vector<pair<string, ll>> &ComplexityToDebruijn::getSubseqToDebruijn() const {
--- Solution/main.cpp
+++ Solution/main.cpp
@@ -9,19 +9,19 @@ using namespace std::chrono;
 using namespace std;
 
 int main(){
-    omp_set_num_threads(12);
+    omp_set_num_threads(8);
     cout << "Starting..." << endl;
-    auto start = high_resolution_clock::now();
-    SequenceGenerator se(19);
-    auto d = se.getSequences();
-    cout << se.getNumOfSeq() << endl;
-    for (int i = 0; i < d.size(); ++i) {
-        cout << d[i] << endl;
-    }
-    auto end = high_resolution_clock::now();
-    auto duration= duration_cast<seconds>(end - start);
-    cout << duration.count() << " seconds" << endl;
-    return 0;
+//    auto start = high_resolution_clock::now();
+//    SequenceGenerator se(19);
+//    auto d = se.getSequences();
+//    cout << se.getNumOfSeq() << endl;
+//    for (int i = 0; i < d.size(); ++i) {
+//        cout << d[i] << endl;
+//    }
+//    auto end = high_resolution_clock::now();
+//    auto duration= duration_cast<seconds>(end - start);
+//    cout << duration.count() << " seconds" << endl;
+//    return 0;
     int order = 7;
     for(int c = 73; c <= 73; c++){
         auto start = high_resolution_clock::now();
