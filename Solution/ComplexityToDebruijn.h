531206436f40c7a7f941ce15d3d11c4edae2a49a
Joan Jozen <52244617+joannj35@users.noreply.github.com>
2023-08-07 21:19:40 +0300

    GPU offloading - strings and vectors -> char* and arrays - declaring targets (methods used by gpu) - added gpu parallelism

--- Solution/ComplexityToDebruijn.h
+++ Solution/ComplexityToDebruijn.h
@@ -21,18 +21,15 @@ class ComplexityToDebruijn {
     int order;
     int sub_complexity;
     ll total_seq_num;
-    vector<vector<string>> up_to_1000;
+    vector<char**> up_to_1000;
     vector<pair<string, ll>> subseq_to_debruijn; //a map that maps the number of De bruijn sequences(value) to the sub seq that it came from
-    void generateXORStrings(const string& s, string& a, string& b, int index, vector<pair<string,string>>& options, vector<bool> check, vector<string>& db_seq);
-    vector<pair<string,string>>  getAllXORStrings(string s, vector<string>& db_seq);
+    void generateXORStrings(const char* s, char*& a, char*& b, int index, char**& options, bool* check, char**& db_seq, ll& options_size);
+    char**  getAllXORStrings(const char* s, char**& db_seq, ll& options_size);
 public:
     ComplexityToDebruijn(int complexity, int order);
     void compute();
-    ll fromSubseqToDebruijn(string seq, vector<string>& db_seq);
-    static bool isRotation(const std::string& s1, std::string s2);
-    vector<string> removeRotations(const vector<string>& sequences);
-
-    const vector<vector<string>> &getUpTo1000() const;
+    ll fromSubseqToDebruijn(const char* seq, char**& db_seq);
+    static bool isRotation(const char* s1, const char* s2);
 
     ll getTotalSeqNum() const;
 
