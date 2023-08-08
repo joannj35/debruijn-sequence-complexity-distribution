3483e76fa3ab17d3d9c4a93a131afd8d5ffb0756
Joan Jozen <52244617+joannj35@users.noreply.github.com>
2023-08-08 17:48:18 +0300

    - fixed rotation error - changed options size to 6000

--- Solution/ComplexityToDebruijn.h
+++ Solution/ComplexityToDebruijn.h
@@ -21,10 +21,10 @@ class ComplexityToDebruijn {
     int order;
     int sub_complexity;
     ll total_seq_num;
-    vector<char**> up_to_1000;
+    char** up_to_1000;
     vector<pair<string, ll>> subseq_to_debruijn; //a map that maps the number of De bruijn sequences(value) to the sub seq that it came from
     void generateXORStrings(const char* s, char*& a, char*& b, int index, char**& options, bool* check, char**& db_seq, ll& options_size);
-    char**  getAllXORStrings(const char* s, char**& db_seq, ll& options_size);
+    ll  getAllXORStrings(const char* s, char**& db_seq, ll& options_size);
 public:
     ComplexityToDebruijn(int complexity, int order);
     void compute();
